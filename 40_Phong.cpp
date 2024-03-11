ddd397c4ce372b3c6c480da45e292c317b85093d
Phong <nguyentv2402@email.com>
2024-03-11 23:07:27 +0700

    them file 40_Phong.cpp vao nhanh 40Nhanh

--- 40_Phong.cpp
+++ 40_Phong.cpp
@@ -1,5 +1,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World1!" << std::endl;
+
+    std::cout << "Hello, World2!" << std::endl;
+    std::cout << "Hello!" << std::endl;
     return 0;
 }
